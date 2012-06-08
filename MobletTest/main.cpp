6403ae882a6b91457ade13657da78f71d01c4887
Luuk <kukel12@hotmail.com>
2012-06-08 19:19:41 +0200

    Dit is Moblet test commit

---
+++ MobletTest/main.cpp
@@ -0,0 +1,61 @@
+#include <MAUtil/Moblet.h>
+#include <conprint.h>
+
+using namespace MAUtil;
+
+/**
+ * A Moblet is a high-level class that defines the
+ * behaviour of a MoSync program.
+ */
+class MyMoblet : public Moblet
+{
+public:
+	/**
+	 * Initialize the application in the constructor.
+	 */
+	MyMoblet()
+	{
+		printf("Press zero or back to exit\n");
+	}
+
+	/**
+	 * Called when a key is pressed.
+	 */
+	void keyPressEvent(int keyCode, int nativeCode)
+	{
+		if (MAK_BACK == keyCode || MAK_0 == keyCode)
+		{
+			// Call close to exit the application.
+			close();
+		}
+
+		// Print the key character.
+		printf("You typed: %c\n", keyCode);
+	}
+
+	/**
+	 * Called when a key is released.
+	 */
+	void keyReleaseEvent(int keyCode, int nativeCode)
+	{
+	}
+
+	/**
+	 * Called when the screen is touched.
+	 */
+	void pointerPressEvent(MAPoint2d point)
+	{
+		// Print the x and y coordinate.
+		printf("You touched: %i %i\n", point.x, point.y);
+	}
+};
+
+/**
+ * Entry point of the program. The MAMain function
+ * needs to be declared as extern "C".
+ */
+extern "C" int MAMain()
+{
+	Moblet::run(new MyMoblet());
+	return 0;
+}
